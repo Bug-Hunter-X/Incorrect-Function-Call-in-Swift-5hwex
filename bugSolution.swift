27229ad0b49135bbf4248f4e03b63122bd2e7413
BugHunter-X <askzenai@gmail.com>
2024-12-24 15:56:04 +0100

    Add bugSolution.swift file

---
+++ bugSolution.swift
@@ -0,0 +1,8 @@
+func calculateArea(length: Double, width: Double) -> Double {
+  return length * width
+}
+
+let area = calculateArea(length: 10, width: 5) // Correct usage
+
+// Corrected call with named parameters
+let correctArea = calculateArea(length: 10, width: 5)
